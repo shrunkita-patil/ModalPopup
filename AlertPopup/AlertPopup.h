6fc5fcc505f3509b0a4c3d4f59b5d50fb8c1600a
shivdas-OSP <shivdas@osplabs.com>
2020-09-01 20:11:23 +0530

    initial commit

---
+++ AlertPopup/AlertPopup.h
@@ -0,0 +1,19 @@
+//
+//  AlertPopup.h
+//  AlertPopup
+//
+//  Created by Riken Shah on 01/09/20.
+//  Copyright © 2020 Shrunkita. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlertPopup.
+FOUNDATION_EXPORT double AlertPopupVersionNumber;
+
+//! Project version string for AlertPopup.
+FOUNDATION_EXPORT const unsigned char AlertPopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertPopup/PublicHeader.h>
+
+
